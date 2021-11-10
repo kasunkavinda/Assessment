59ff6c1dcc5e31e1b86aaea8be67a87696b0b3f7
Kasun <kasunkavinda.bc@gmail.com>
2021-11-10 07:21:07 +0530

    first commit

---
+++ components/navigation/Navigation.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Grid from "@material-ui/core/Grid";
+import classes from "./Navigation.module.css";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  MenuIcon,
+  Typography,
+  Button,
+} from "@material-ui/core";
+
+const Navigation = () => {
+  return (
+    <div className={classes.navwrapper}>
+      <Grid container spacing={3}>
+        <AppBar position="static">
+          <Toolbar className={classes.backgroundColor}>
+            <Grid item sm={10}>
+              <Typography variant="h6" className={classes.title}>
+                Classfied Ads
+              </Typography>
+            </Grid>
+            <Grid item sm={1}>
+              <Typography variant="h6" className={classes.home}>
+                HOME
+              </Typography>
+            </Grid>
+            <Grid item sm={1}>
+              <Typography variant="h6" className={classes.favorites}>
+                FAVORITES
+              </Typography>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+    </div>
+  );
+};
+
+export default Navigation;
---
+++ pages/api/new-card.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    //expects this type of object
+    const { category, title, description, id } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://kasunkavinda:kasunkavinda456123@cluster0.oxj4i.mongodb.net/myPortfolio?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+    const cardCollection = db.collection("CardCollection");
+    const result = await cardCollection.insertOne(data);
+    client.close();
+
+    res.status(201).json({ messgae: "Card Inserted" });
+  }
+}
+
+export default handler;
